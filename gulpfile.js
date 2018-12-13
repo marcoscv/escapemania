02ae36c7781ae2b4e4804851a8ede74ba4b93565
Marcos Castro <marcoscv@gmail.com>
2018-12-13 13:27:20 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const cleanCSS = require('gulp-clean-css');
 const ghPages = require('gulp-gh-pages');
+const imagemin = require('gulp-imagemin');
+ 
+gulp.task('images', () =>
+    gulp.src('src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('src/images'))
+);
 
 // compile custom SCSS files
 gulp.task('sass', () => {
@@ -33,7 +40,7 @@ gulp.task('pug', function() {
 
 
 // copy JS modules
-gulp.task('js', () => {
+gulp.task('copy-js', () => {
   return gulp.src([
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/jquery/dist/jquery.min.js',
@@ -43,35 +50,25 @@ gulp.task('js', () => {
   .pipe(browserSync.stream());
 });
 
-// copy CSS static modules
-gulp.task('css', () => {
-  return gulp.src([
-    'node_modules/animate.css/animate.min.css'
-  ])
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
-});
-
-
-
 // init develop Server
-gulp.task('serve', ['sass','js','css','pug'], () => {
+gulp.task('serve', ['sass','copy-js', 'pug', 'images'], () => {
   browserSync.init({
     server: './src'
   });
   
   gulp.watch(['src/templates/**/*.pug','src/templates/**/*.txt'], ['pug']);
   gulp.watch(['src/scss/*.scss'], ['sass','minify-css']);
+  gulp.watch(['src/images/**/*'], ['images']);
   gulp.watch('src/**/*').on('change',
     browserSync.reload
   );
 });
 
 //Publish gh-pages
-gulp.task('deploy', function() {
+gulp.task('deploy', ['sass','copy-js', 'pug', 'images'], () => {
   return gulp.src('./src/**/*')
     .pipe(ghPages());
 });
 
 // defaul task typing 'gulp'
-gulp.task('default', ['js', 'css', 'sass', 'minify-css', 'serve'])
+gulp.task('default', ['copy-js',   'sass', 'minify-css', 'serve'])
