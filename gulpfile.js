c4776f3aafc477f0eb1c5327a87510c57995ec2d
Marcos Castro <marcoscv@gmail.com>
2018-12-15 00:54:17 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,29 @@ const pug = require('gulp-pug');
 const cleanCSS = require('gulp-clean-css');
 const ghPages = require('gulp-gh-pages');
 const imagemin = require('gulp-imagemin');
- 
-gulp.task('images', () =>
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+
+// optimize images
+gulp.task('minify-images', () =>
     gulp.src('src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('src/images'))
 );
+
+//minify JS in a single 
+var jsFiles = 'src/js/*.js',
+    jsDest = 'src/js/jsOneFile';
+
+gulp.task('minify-js', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
+});
 
 // compile custom SCSS files
 gulp.task('sass', () => {
@@ -31,13 +48,12 @@ gulp.task('minify-css', () => {
 gulp.task('pug', function() {  
   return gulp.src('src/templates/*.pug')
   .pipe(pug({
-    pretty: true,
-    cache: false
+    pretty: false,
+    cache: true
   }))
   .pipe(gulp.dest('src'))
   .pipe(browserSync.stream());
 });
-
 
 // copy JS modules
 gulp.task('copy-js', () => {
@@ -51,24 +67,25 @@ gulp.task('copy-js', () => {
 });
 
 // init develop Server
-gulp.task('serve', ['sass','copy-js', 'pug', 'images'], () => {
+gulp.task('serve', ['sass','pug', 'minify-images'], () => {
   browserSync.init({
     server: './src'
   });
   
   gulp.watch(['src/templates/**/*.pug','src/templates/**/*.txt'], ['pug']);
   gulp.watch(['src/scss/*.scss'], ['sass','minify-css']);
-  gulp.watch(['src/images/**/*'], ['images']);
+  gulp.watch(['src/images/**/*'], ['minify-images']);
+  gulp.watch(['src/js/**/*'], ['minify-js']);
   gulp.watch('src/**/*').on('change',
     browserSync.reload
   );
 });
 
 //Publish gh-pages
-gulp.task('deploy', ['sass','copy-js', 'pug', 'images'], () => {
+gulp.task('deploy', ['sass', 'pug', 'minify-js', 'minify-css', 'minify-images'], () => {
   return gulp.src('./src/**/*')
     .pipe(ghPages());
 });
 
 // defaul task typing 'gulp'
-gulp.task('default', ['copy-js',   'sass', 'minify-css', 'serve'])
+gulp.task('default', ['sass', 'minify-js', 'minify-css', 'minify-images', 'serve'])
