a2eda006dbd0ff044de4dc5f89ecd761a03ae1b0
Marcos Castro <marcoscv@gmail.com>
2018-12-16 19:54:53 +0100

    Update 2018-12-16T18:54:51.880Z

--- js/5_main.js
+++ js/5_main.js
@@ -35,7 +35,7 @@ $.fn.extend({
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
           $('html, body').animate({
-            scrollTop: (target.offset().top - 70)
+            scrollTop: (target.offset().top - 65)
           }, 1000, "easeInOutExpo");
           return false;
         }
